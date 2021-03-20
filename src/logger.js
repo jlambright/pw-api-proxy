4dd7f6e098bc5452ad6c1017f4e603a53c138087
Josh Lambright <joshua.lambright@gmail.com>
2021-03-20 19:54:14 -0400

    Updated logging config.

--- src/logger.js
+++ src/logger.js
@@ -4,13 +4,25 @@ const {createLogger, format, transports} = require('winston');
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
   level: 'info',
-  format: format.combine(
-    format.splat(),
-    format.simple()
-  ),
+  format: format.json(),
   transports: [
-    new transports.Console()
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
   ],
 });
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
+}
 
 module.exports = logger;
