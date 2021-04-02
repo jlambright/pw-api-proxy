605e0e67821ead19ad55ea51a7a0310bc712a2e9
Josh Lambright <joshua.lambright@gmail.com>
2021-04-02 06:40:55 -0400

    Debugging line.

--- src/auth.js
+++ src/auth.js
@@ -5,6 +5,8 @@ const logger = require("./logger");
 
 const fbKey = JSON.parse(process.env.FIREBASE_KEY);
 
+logger.info(fbKey);
+
 const credential = admin.credential.cert({
     projectId: fbKey["project_id"], // I get no error here
     clientEmail: fbKey["client_email"], // I get no error here
