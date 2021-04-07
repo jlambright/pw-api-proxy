85808b121f4b6dafbaccd13cd0086d3e6b43d955
Josh Lambright <joshua.lambright@gmail.com>
2021-04-07 04:23:09 -0400

    Going the functional programming route.

--- src/roundmap.js
+++ src/roundmap.js
@@ -6,34 +6,32 @@ const logger = require("./logger");
 const key = datastore.key(["Active", "state"]);
 const stateQuery = datastore.createQuery("Active").filter("__key__", key);
 
-class RoundMap extends Object{
-    matchups = {};
-    stories = {};
+const RoundMap = (matchupArray) => {
+    let matchups = {};
+    let stories = {};
 
-    constructor(matchupArray) {
-        super();
-        let self = this;
-        Object.entries(matchupArray).forEach((entry) => {
-            const {key, value} = entry;
-            const aStoryID = value["a-story"];
-            const bStoryID = value["b-story"];
-            const voters = value.hasOwnProperty("voters") ? value.voters : [];
-            const storyArray = [value["a-story"], value["b-story"]];
-            self.matchups[key] = {
-                "a-story": aStoryID,
-                "b-story": bStoryID,
-                voters: voters,
-            };
-            self.stories[aStoryID] = {
-                matchID: key,
-                slot: this.matchups[key][value["a-story"]]
-            };
-            self.stories[bStoryID] = {
-                matchID: key,
-                slot: this.matchups[key][value["b-story"]]
-            }
-        });
-    }
+    Object.entries(matchupArray).forEach((entry) => {
+        const {key, value} = entry;
+        const aStoryID = value["a-story"];
+        const bStoryID = value["b-story"];
+        const voters = value.hasOwnProperty("voters") ? value.voters : [];
+        const storyArray = [value["a-story"], value["b-story"]];
+        self.matchups[key] = {
+            "a-story": aStoryID,
+            "b-story": bStoryID,
+            voters: voters,
+        };
+        self.stories[aStoryID] = {
+            matchID: key,
+            slot: this.matchups[key][value["a-story"]]
+        };
+        self.stories[bStoryID] = {
+            matchID: key,
+            slot: this.matchups[key][value["b-story"]]
+        }
+    });
+
+    return {matchups, stories};
 }
 
 module.exports.build = () => datastore.runQuery(stateQuery)
